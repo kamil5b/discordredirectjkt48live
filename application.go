4165e8c9404460de8fdda3a098e668d5474af5d9
Sayang Kamil5b <79760043+kamil5b@users.noreply.github.com>
2023-12-26 21:19:37 +0700

    initial

---
+++ application.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	// Initialize the application
+	app := fiber.New()
+
+	// Hello, World!
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+	app.Get("/discord/jkt48live", func(c *fiber.Ctx) error {
+		return c.Redirect("https://discord.com/api/oauth2/authorize?client_id=1141780829933158420&permissions=51200&scope=bot")
+	})
+
+	// Listen and Server in 0.0.0.0:$PORT
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000"
+	}
+
+	log.Fatal(app.Listen(":" + port))
+}
